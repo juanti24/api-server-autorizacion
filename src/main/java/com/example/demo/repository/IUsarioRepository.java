5c242d3b8dced9ce73b3a7726a5fa4f9c8ebc786
Juan Jumbo <tiniyan24@gmail.com>
2024-02-21 20:47:45 -0500

    first commit

---
+++ src/main/java/com/example/demo/repository/IUsarioRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.repository;
+
+import com.example.demo.repository.modelo.Usuario;
+
+public interface IUsarioRepository {
+
+	public Usuario consultarPorNombre(String nombre);
+	
+
+}
